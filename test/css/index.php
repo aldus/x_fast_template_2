7cd396147c4e98b8ee8a46c0db04f67fa782ef82
Dietrich <dietrich@powermac-g4.speedport.ip>
2013-01-12 10:36:33 +0100

    add some index.php files and missing simple_source.lte

---
+++ test/css/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *	@version	0.6.2
+ *	@date		2013-01-10
+ *	@author		Dietrich Roland Pehlke
+ *	@package	Lepton-CMS - Modules: xFastTemplate2
+ *
+ */
+header('Location: ../../index.php');
+
+?>
