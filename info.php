f8143a38e88d66806850669c8b47d543970dfee0
Dietrich <dietrich@powermac-g4.speedport.ip>
2013-01-14 12:14:23 +0100

    update info.php and changelog

--- info.php
+++ info.php
@@ -14,7 +14,7 @@ $module_version       = "0.6.2";
 $module_platform      = (!defined('LEPTON_GUID')) ? "2.8" : "1.1.0";
 $module_author        = "Dietrich Roland Pehlke (Aldus).";
 $module_license       = "GNU General Public License";
-$module_description   = "Implantation of drp-xFastTemplate2. see <a href=\"http://websitebakers.org/_documentation/x_fastTemplate/index.html\" target=\"_blank\">documentation on Websitebakers.org</a>. An example-template can be download <a target='blank' href='http://websitebakers.org/_documentation/x_fastTemplate/aldus_simple.zip'> here </a>.";
+$module_description   = "Implantation of drp-xFastTemplate2. see <a href=\"http://drp-x-work.de/lepton-cms/docs/x_fast_template_2/index.html\" target=\"_blank\">documentation</a>. An example-template can be download <a target='blank' href='http://websitebakers.org/_documentation/x_fastTemplate/aldus_simple.zip'> here </a>.";
 $module_guid		  = "7588B558-135A-4F6A-9263-9E3102D5EF61";
 
 ?>
